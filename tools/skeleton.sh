1089b852c20069161a43cbe33937a0e29a305522
Florian <51517706+Florianb63@users.noreply.github.com>
2025-08-01 15:46:27 +0200

    Update skeleton.sh

--- tools/skeleton.sh
+++ tools/skeleton.sh
@@ -6,7 +6,7 @@ read plugin_name
 
 # replace skeleton with the name of the plugin
 echo "Replacing skeleton with $plugin_name"
-find ./inc/ ./front/ -type f -exec sed -i "s/skeleton/$plugin_name/g" {} \;
+find ./inc/ ./front/ ./.github/workflows -type f -exec sed -i "s/skeleton/$plugin_name/g" {} \;
 sed -i "s/skeleton/$plugin_name/g" setup.php hook.php
 
 # replace Skeleton with the name of the plugin with a capital letter
